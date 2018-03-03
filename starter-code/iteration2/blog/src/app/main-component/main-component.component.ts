8346c8465951638b99309f0d0967b32bb3b0ecf0
sevi221 <sevilaura221@gmail.com>
2018-03-03 14:42:06 +0100

    second commit half second iteration

--- starter-code/iteration2/blog/src/app/main-component/main-component.component.ts
+++ starter-code/iteration2/blog/src/app/main-component/main-component.component.ts
@@ -5,7 +5,6 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <h1>Learn how to cook brisket meat</h1>
     <div>
-    <img src="./img/meat.jpg">
     <p>"Brisket is a cut of meat from the breast or lower chest of beef or veal. The beef brisket is one of the nine beef primal cuts, though the precise definition of the cut differs internationally. The brisket muscles include the superficial and deep pectorals. As cattle do not have collar bones, these muscles support about 60% of the body weight of standing/moving cattle. This requires a significant amount of connective tissue, so the resulting meat must be cooked correctly to tenderize the connective tissue.According to the Random House Dictionary of the English Language, Second Edition, the term derives from the Middle English brusket which comes from the earlier Old Norse brjósk, meaning cartilage. The cut overlies the sternum, ribs, and connecting costal cartilages."</p>
     </div>
 
